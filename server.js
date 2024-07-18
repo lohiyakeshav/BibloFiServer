a3146053465d536cd7ba874ab71f25eebb5dd791
Keshav Lohiya <keshav.lohiyas@gmail.com>
2024-07-18 15:01:33 +0530

    Server Tweaks

--- server.js
+++ server.js
@@ -18,10 +18,12 @@ const transporter = nodemailer.createTransport({
 });
 
 app.post('/send-email', (req, res) => {
-    console.log("Sending")
-    const { email, password, name } = req.body;
+    console.log("Sending");
+    const { email, password, name , } = req.body;
+    console.log(email, password, name);
 
-    console.log(email, password, name )
+    
+
     // Read the HTML template file
     const templatePath = path.join(__dirname, 'public', 'emailtemplate.html');
     fs.readFile(templatePath, 'utf8', (err, data) => {
@@ -29,12 +31,23 @@ app.post('/send-email', (req, res) => {
             return res.status(500).send('Error reading email template.');
         }
 
+        // const imagePath = path.join(__dirname, 'images', 'banner.png');
+        // const base64Image = fs.readFileSync(imagePath, 'base64');
+        // const base64ImageString = `${base64Image}`;
+
+
+        // Replace placeholders with actual values
+        let customizedTemplate = data.replace('{{name}}', name)
+                                     .replace('{{email}}', email)
+                                     .replace('{{password}}', password);
+                                     //.replace('{{base64Image}}', base64ImageString);
+
         // Customize the email content with user-specific details
         const mailOptions = {
             from: 'keshavlohiyabusiness@gmail.com',
             to: email,
             subject: 'Librarian Credentials',
-            html: data.replace('{{name}}', name) // Replace template placeholder with actual name
+            html: customizedTemplate
         };
 
         // Send email
